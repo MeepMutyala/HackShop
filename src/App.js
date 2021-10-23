bf309afb23f6a30c2f51c9f2857bfbff4c11b55d
sarang-pujari <sarangpujari01@gmail.com>
2021-10-23 13:53:56 -0400

    add firebase functionality

--- src/App.js
+++ src/App.js
@@ -1,34 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
 import React from "react";
+
+// import React, { useRef } from 'react'
+// import { Form, Button, Card } from 'react-bootstrap'
+import Signup from "./signup.js"
+import Login from "./Login.js"
+import Dashboard from "./Dashboard.js"
+
+
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from "react-router-dom";
+import { AuthProvider } from './contexts/AuthContext'; 
 
 function App() {
   return (
+    
     <Router> {/*this is a JSX comment, note the syntax*/}
-      <div className="Home">  {/*this is our home page, this div contains everything in our home page*/}
+    <AuthProvider>
+      {/* this is our home page, this div contains everything in our home page */}
 
       <Switch>
         <Route exact path="/">
           <Home />
         </Route>  
 
-        <Route exact path="/">
-          <Game />
+        <Route exact path="/dashboard">
+          <Dashboard />
         </Route>  
         
-        <Route exact path="/leaderboard">
-          <Leaderboard />
+        <Route exact path="/signup">
+          <Signup />
         </Route>  
+
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        
         
       </Switch>
 
-      </div>
+
+      </AuthProvider>
     </Router>
     
   );
@@ -42,8 +59,9 @@ function Home() {
           <p>
             Welcome to <code>HackShop!</code>    <br/>
             <Link to="/">This is a link to the homepage!</Link><br/>
-            <Link to="/game">This is a link to a game!</Link><br/>
-            <Link to="/leaderboard">This is a link to a leaderboard!</Link>
+            <Link to="/dashboard">This is a link to a dashboard!</Link><br/>
+            <Link to="/signup">This is a link to a create account!</Link><br/>
+            <Link to="/login">This is a link to a login</Link>
           </p>
           <a
             className="App-link"
@@ -58,20 +76,5 @@ function Home() {
   );
 }
 
-function Game() {
-  return (
-    <div>
-      <h2>About</h2>
-    </div>
-  );
-}
-
-function Leaderboard() {
-  return (
-    <div>
-      <h2>Dashboard</h2>
-    </div>
-  );
-}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
+
+import "bootstrap/dist/css/bootstrap.min.css"
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import Signup from "./signup.js"
+import Login from "./Login.js"
+import Dashboard from "./Dashboard.js"
+
+
+
+const Routing = () => {
+  return(
+    <Router>
+        <Route exact path="/" component={App} />
+        <Route exact path="./signup" component={Signup} />
+        <Route exact path="./Login" component={Login} />
+        <Route exact path="./Dashboard" component={Dashboard} />
+    </Router>
+  )
+}
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Routing />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
